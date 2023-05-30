0ca81e0f3f3b9da9c279906689b72b9734f9c425
584631935@qq.com <584631935@qq.com>
2023-05-30 14:21:54 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -12,28 +12,7 @@
 */
 
 Route::get('/', function () {
-    // 微信token认证
-    $signature = $_GET["signature"];
-    $timestamp = $_GET["timestamp"];
-    $nonce     = $_GET["nonce"];
-    $echostr   = $_GET["echostr"];
-
-    // 你在微信公众号后台的设置的Token
-    $token = "R3QGOqgF4LAje9M7HJAR1w1LM9kJr4wT";
-
-    // 1）将token、timestamp、nonce三个参数进行字典序排序
-    $tmpArr = array($nonce, $token, $timestamp);
-    sort($tmpArr, SORT_STRING);
-
-    // 2）将三个参数字符串拼接成一个字符串进行sha1加密
-    $str  = implode($tmpArr);
-    $sign = sha1($str);
-
-    // 3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-    if ($sign == $signature) {
-        echo $echostr;
-    }
-    // return null;
+    return 'Welcome LaravelAdmin';
 });
 
 Route::get("/ddoc","Controller@getDdoc");                                    // 获取数据字典
